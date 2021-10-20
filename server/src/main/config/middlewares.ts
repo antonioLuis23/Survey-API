7edeb855f4ec1281564d286bdc0dd4d28bbc45f9
Antonio <antonio_luis@outlook.com>
2021-10-19 22:33:47 -0300

    feat: ensure cors is enabled

--- server/src/main/config/middlewares.ts
+++ server/src/main/config/middlewares.ts
@@ -1,6 +1,8 @@
 import { Express } from 'Express';
 import { bodyParser } from '../middlewares/body-parser';
+import { cors } from '../middlewares/cors';
 
 export default (app: Express): void => {
   app.use(bodyParser);
+  app.use(cors);
 };
